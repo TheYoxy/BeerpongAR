e3ce8f86518c7b06147b8c90b2049e041b136034
Upd4ting <upd4ting@gmail.com>
2019-04-28 02:18:46 +0200

    fix collision

--- Assets/Script/Inside.cs
+++ Assets/Script/Inside.cs
@@ -8,22 +8,6 @@ using HoloToolkit.Sharing.SyncModel;
 
 public class Inside : MonoBehaviour {
     private const string    TAGBALL = "Ball";
-    private       Coroutine Routine;
-
-    private IEnumerator Oui(GameObject gobelet) {
-        yield return new WaitForSeconds(.5f);
-
-        if (StateRegistrer.Instance.crt != null)
-            StopCoroutine(StateRegistrer.Instance.crt);
-
-        SyncObjectSpawner _spawner = GameObject.Find("SyncObjectSpawner").GetComponent<SyncObjectSpawner>();
-
-        SyncBall ball = (SyncBall) _spawner.SearchSyncObject(typeof(SyncBall));
-        _spawner.DeleteSyncObject(ball);
-
-        StateRegistrer.Instance.game.desactivedObjects.AddObject(new SyncObjectString(gobelet.transform.GetFullPath()));
-        StateRegistrer.Instance.game.playerTurn.Value = !StateRegistrer.Instance.game.playerTurn.Value;
-    }
 
     private void Start() { }
 
@@ -31,13 +15,16 @@ public class Inside : MonoBehaviour {
 
     private void OnTriggerEnter(Collider collider) {
         if (collider.tag == TAGBALL)
-            Routine = StartCoroutine(Oui(transform.parent.gameObject));
+        {
+            SyncObjectSpawner _spawner = GameObject.Find("SyncObjectSpawner").GetComponent<SyncObjectSpawner>();
+
+            SyncBall ball = (SyncBall)_spawner.SearchSyncObject(typeof(SyncBall));
+            _spawner.DeleteSyncObject(ball);
+
+            StateRegistrer.Instance.game.desactivedObjects.AddObject(new SyncObjectString(gobelet.transform.GetFullPath()));
+            StateRegistrer.Instance.game.playerTurn.Value = !StateRegistrer.Instance.game.playerTurn.Value;
+        }
     }
 
     private void OnTriggerStay(Collider collider) { }
-
-    private void OnTriggerExit(Collider collider) {
-        if (collider.tag == TAGBALL)
-            StopCoroutine(Routine);
-    }
 }
